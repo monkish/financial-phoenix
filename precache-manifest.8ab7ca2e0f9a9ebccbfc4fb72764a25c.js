3fbc5c60b3847c84f71e80ad6f7297c1559ec7bf
John Carey <jcarey1973@gmail.com>
2019-06-09 18:25:44 -0400

    Updates

---
+++ precache-manifest.8ab7ca2e0f9a9ebccbfc4fb72764a25c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d2eaad37f20bcbc0d13b303c373e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f8bd9407c463e99dc0e",
+    "url": "/static/css/main.3f4385af.chunk.css"
+  },
+  {
+    "revision": "52d16aad0d3301959056",
+    "url": "/static/js/2.34d3b3ac.chunk.js"
+  },
+  {
+    "revision": "6f8bd9407c463e99dc0e",
+    "url": "/static/js/main.cf176698.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
